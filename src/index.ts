92b65af77255789c28ac5a46eeebf20403ad4a17
jake-the-cake <askjake331@gmail.com>
2023-11-27 13:17:10 -0500

    connect to express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hi')
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+const app = express()
+
+app.listen(5000, callbackLog('connected'))
+
+function callbackLog(message: string): () => void {
+  return () => console.log(message)
+}
