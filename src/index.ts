e57f55a5594c1725ad08bb5f91b7f427235199a0
jake-the-cake <askjake331@gmail.com>
2023-11-27 19:46:59 -0500

    start layout

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
-import express from 'express'
+import express, { Request } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import fs from 'fs'
+import path from 'path'
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 
-app.listen(5000, callbackLog('connected'))
+app.get('/', (req, res) => {
 
-function callbackLog(message: string): () => void {
-  return () => console.log(message)
+  const rawHtml = Buffer.from(fs.readFileSync(path.join(__dirname, '..', 'public','index.html'))).toString('utf-8')
+
+  const html = insertMetaData(rawHtml, {})
+
+  res.send(html)
+})
+
+app.listen(5000, messageLog('connected'))
+
+function insertMetaData(html: string, data: any): string {
+  return html
 }
+
+function messageLog(message: string): () => void { return () => console.log(message)}
+function messageWarn(message: string): () => void { return () => console.log(message)}
+function msgError(message: string): () => void { return () => console.log(message)}
